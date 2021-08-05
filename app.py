f4b6dd5ab8acf0bd4ce33446aef5114348a8ec7f
Joshua Golafshan <JGolafshan@outlook.com>
2021-08-05 22:14:00 +1000

    style sheet moved to its own folders, started setting out the html elements

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from alpha_vantage.fundamentaldata import FundamentalData
 import pandas as pd
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+import plotly.express as px
 import dash
 import dash_core_components as dcc
 import dash_bootstrap_components as dbc
@@ -10,6 +11,8 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 from datetime import date
 from datetime import datetime as dt
+import os
+from pprint import pprint
 
 # Alpha Vantage instantiation
 av_key = "EPX9O905XDNWU29K"
@@ -19,32 +22,93 @@ fd = FundamentalData(key=av_key, output_format="json")
 # select stock ticker and create AV objects
 stock = "IBM"
 fd_real = fd.get_company_overview(stock).__getitem__(0)
+ts_real = ts.get_weekly(stock).__getitem__(0)
+ts_real = pd.DataFrame.from_records(ts_real)
+pprint(fd_real)
 
 # Dash app instantiation
-external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css", dbc.themes.GRID, "E:\WallStreetBets_Analysis\stylesheet.css"]
-app = dash.Dash("Wall-Street Social", external_stylesheets=external_stylesheets)
-app.title = "Wall-Street Social"
+external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css",
+                        dbc.themes.GRID,
+                        "https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"]
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets, title="Wall-Street Social")
+
+server = app.server
+app.config.suppress_callback_exceptions = True
+app.css.config.serve_locally = True
+app.scripts.config.serve_locally = True
 
 # Acaully dash app
-fig = go.Figure(data=[go.Scatter(x=[1, 2, 3], y=[4, 1, 2])])
-app.layout = html.Div([
-    html.Div([
-        dbc.Row([
-            dbc.Col([html.P( f"Full Name: {fd_real.get('Name')}")]),
-            dbc.Col([html.P(f"Symbol: {fd_real.get('Symbol')}")])
-        ], align="baseline"),
 
-        dbc.Row([
-            dbc.Col([html.P(f"Asset Type: {fd_real.get('AssetType')}")]),
-            dbc.Col([html.P(f"Sector: {fd_real.get('Sector')}")]),
-            dbc.Col([html.P(f"Industry Group: {fd_real.get('Industry')}")])
-        ], align="baseline")
-    ]),
-    dcc.Graph(
-        id="basic",
-        figure=fig
-    )
-], className="container")
+app.layout = html.Div(
+    className="container-lg",
+    children=[
+
+        # Stock Info
+        html.Div(
+            className="app-header",
+            children=[
+                dbc.Row([
+                    dbc.Col([html.H2(fd_real.get('Name'))]),
+                    dbc.Col([html.H2(fd_real.get('Symbol'))])
+                ], align="baseline"),
+            ]),
+
+        # Stock Graph
+        html.Div(
+            className="graph",
+            children=[
+                dcc.Graph(
+                    id="basic",
+                    figure=go.Figure(data=[go.Scatter(x=[1, 2, 3], y=[4, 1, 2])])
+                )]
+        ),
+
+        # Share Fundamentals
+        html.Div(
+            className="app-adv",
+            children=[
+                dbc.Row([
+                    dbc.Col([
+                        html.H4('SHARE INFORMATION')
+                    ]),
+                    dbc.Col([
+                        html.H4('FUNDAMENTALS')
+                    ]),
+
+                    dbc.Col([
+                        html.H4('DIVIDENDS')
+                    ]),
+                ], align="baseline"),
+            ]),
+
+        # NewsFeed
+        html.Div(
+            className="app-adv",
+            children=[
+                dbc.Row([
+                    html.H4('NEWS FEED'),
+                    dbc.Row([
+                        html.P("Blashjsadjadshjsad"),
+                        html.P("date: 2021/08/5"),
+                    ]),
+                ], align="baseline"),
+
+            ]),
+
+        # NewsFeed
+        html.Div(
+            className="app-adv",
+            children=[
+                dbc.Row([
+                    html.H4('ANNOUNCEMENTS'),
+                    dbc.Row([
+                        html.P("Blashjsadjadshjsad"),
+                        html.P("date: 2021/08/5"),
+                    ]),
+                ], align="baseline"),
+
+            ]),
+    ])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
