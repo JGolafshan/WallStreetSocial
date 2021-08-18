e5745478a37aec566f66bf4d4937465b294d1780
Joshua Golafshan <JGolafshan@outlook.com>
2021-08-18 10:46:57 +1000

    database Automation added, a few errors need to worked out for example.

--- app.py
+++ app.py
@@ -22,6 +22,22 @@ def millify(n):
 symbol = "AAPL"
 ticker = yf.ticker.Ticker(ticker=symbol)
 
+from iexfinance import account
+
+api_keys = [
+    "pk_294d45992fbb4e8aa325cae768f6468b",
+    "pk_f74c2c3a28b04fb6b756bb029766860b"
+]
+
+# select stock ticker and create AV objects
+symbol = "TSLA"
+stock = Stock(symbol, token="pk_294d45992fbb4e8aa325cae768f6468b", output_format="json")
+
+
+this = "change"
+
+
+
 ticker_info = ticker.info
 pprint(ticker_info)
 change = round(ticker_info.get('regularMarketPrice') - ticker_info.get('regularMarketPreviousClose'), 3)
