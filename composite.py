5fe746187d59d604faf96ea1a1e74db51424bb8b
Joshua Golafshan <JGolafshan@outlook.com>
2021-08-25 00:40:01 +1000

    ticker.py updated. composite

---
+++ composite.py
@@ -0,0 +1,21 @@
+from database import Database
+from ticker import TickerPipe
+import datetime as dt
+
+db = Database()
+ticker = TickerPipe()
+
+loadData = db.loadCommentBatch(0, 0)
+
+for index, row in loadData.iterrows():
+    check_for_tickler = ticker.find_tickers(row[3])
+    if len(check_for_tickler):
+        db.cursor.execute(
+            f"""
+            INSERT INTO public."Ticker"
+            (CommentID_FK, TickerSymbol, TickerSentiment)
+            VALUES
+            ({row[0]},{str(check_for_tickler[0]).replace("$","")},{0.0})
+            """
+        )
+print(loadData)
