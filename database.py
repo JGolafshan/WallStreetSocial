ec924a338814f93fa1b290bb6cd91de8e5bf9110
Joshua Golafshan <JGolafshan@outlook.com>
2021-10-06 02:37:44 +1100

    composite.py remove, function has been added to database.py, reddit.py doc strings added

--- database.py
+++ database.py
@@ -1,10 +1,16 @@
 import psycopg2
 import os
 import pandas.io.sql as sqlio
+import datetime as dt
+import spacy
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from model_utils.preprocess import preprocess
 
 
 class Database:
-    """"""
+    """
+
+    """
 
     def __init__(self):
         self.conn = psycopg2.connect(
@@ -12,7 +18,9 @@ class Database:
         self.cursor = self.conn.cursor()
 
     def createFromExisting(self, file):
-        """"""
+        """
+
+        """
         dir_name = os.path.dirname(os.path.abspath(__file__))
         folder = "dependencies"
         file = f"{dir_name}\{folder}\{file}"
@@ -22,13 +30,17 @@ class Database:
         self.conn.commit()
 
     def createMerged(self):
-        """"""
+        """
+
+        """
         files = ["createRedditTableSimplified.txt", "createTickerTable.txt"]
         for i in files:
             self.createFromExisting(i)
 
     def redditDump(self, path):
-        """"""
+        """
+
+        """
         with open(path, 'r', encoding='utf-8-sig') as f:
             next(f)
             self.cursor.copy_from(f, 'Comment', sep=',', columns=('CommentAuthor', 'CommentPostDate', 'CommentText'))
@@ -46,4 +58,40 @@ class Database:
         query_params = {'before': before, 'after': after}
         return sqlio.read_sql_query(sql, self.conn)
 
+    def generateTickers(self):
+        """
+        loads data from the redddit table, and generates rows for ticker table
+        """
+        loadData = self.loadCommentBatch(0, 0)
+        wsb = spacy.load("E:/WallStreetSocial/models/wsb_ner")
+        sia = SentimentIntensityAnalyzer()
 
+        # Add to Ticker DB
+        for index, row in loadData.iterrows():
+            text = row[3]
+            doc = wsb(preprocess(text))
+            for en in doc.ents:
+                if (len(doc.ents) >= 1 and row[4] == None):
+                    self.cursor.execute(
+                        f"""
+                            INSERT INTO public."Ticker" ("CommentID_FK", "TickerSymbol", "TickerSentiment")
+                            VALUES ({row[0]}, {"'" + str(en).replace("$", "") + "'"}, {sia.polarity_scores(text)['compound']})
+                        """
+                    )
+                    self.cursor.execute(
+                        f"""
+                            UPDATE public."Comment" 
+                            SET "CommentHasTicker" = true
+                            WHERE "CommentID" = {row[0]}
+                        """
+                    )
+                    self.conn.commit()
+                else:
+                    self.cursor.execute(
+                        f"""
+                            UPDATE public."Comment" 
+                            SET "CommentHasTicker" = false
+                            WHERE "CommentID" = {row[0]}
+                        """
+                    )
+                    self.conn.commit()
--- reddit.py
+++ reddit.py
@@ -20,10 +20,11 @@ class RedditPipe:
         self.subreddit = subreddit
 
     def getRedditComments(self):
-        """Returns a dataframe containing comments from a particular subreddit between
-              given date frame defined by before and after.
-              Before and after variables must be converted to epoch time before calling them as arguments.
-           """
+        """
+        Returns a dataframe containing comments from a particular subreddit between
+        given date frame defined by before and after.
+        Before and after variables must be converted to epoch time before calling them as arguments.
+        """
         api = PushshiftAPI()
         comments = api.search_comments(subreddit=self.subreddit, before=self.before, after=self.after)
         comments_df = pd.DataFrame(comments)
@@ -31,16 +32,23 @@ class RedditPipe:
 
     # noinspection PyMethodMayBeStatic
     def createFinalDataframe(self, df):
-        """"""
+        """
+        Cleans data, removes unwanted fields
+        """
         clean_df = df[['id', 'created_utc', 'body']]
         clean_df['created_utc'] = pd.to_datetime(clean_df['created_utc'], unit='s')
         clean_df = clean_df.replace(',', '', regex=True)
         clean_df = clean_df.replace('\n', '', regex=True)
+        clean_df = clean_df.replace("'", '', regex=True)
+        clean_df = clean_df.replace('"', '', regex=True)
         return clean_df
 
     # noinspection PyMethodMayBeStatic
     def commentsToCsv(self, df):
-        """"""
+        """
+        Converts the dataframe to CSV which is saved for logging/debuging purposes.
+        Which can be found in /dependencies/logs
+        """
         dir_name = os.path.dirname(os.path.abspath(__file__))
         folder = 'temp'
         file_name = 'wsb_comments_' + dt.datetime.now().strftime("%Y_%m_%d_%I_%M")
@@ -50,7 +58,10 @@ class RedditPipe:
         return path
 
     def redditStack(self):
-        """"""
+        """
+        An all in one function which pulls reddit comments puts them in the database along with logging comments
+        This function will be deprecated in the future
+        """
         comments_df = self.getRedditComments()
         final_df = self.createFinalDataframe(comments_df)
         path = self.commentsToCsv(final_df)
